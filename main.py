cf7314fd11122e5c48744469289413ee1101d51e
Cody Codera <armandcycodera@gmail.com>
2025-07-14 20:40:21 +0800

    Can read and speak PDF. Weird bug where it prints out the line from Tim Ferris

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import this
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pyttsx4
+import PyPDF2
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+class BookReader:
+    def __init__ (self, file: str = "") -> None:
+        this.file = file
+        this.total_pages = 0
+
+    def scan_book(self, file_name: str = "./input/Indistractible(Nir Eyal).pdf") -> None:
+        with open(file_name, "rb") as file:
+            pdf_reader = PyPDF2.PdfReader(file)
+            this.total_pages = len(pdf_reader.pages)
+            speaker = pyttsx4.init()
+            page = pdf_reader.pages[20]
+            text = page.extract_text()
+            speaker.say(text)
+            speaker.runAndWait()
+
+    def set_file(self, file_name: str) -> None:
+        this.file = file_name
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    nir_eyal = BookReader()
+    nir_eyal.scan_book()
